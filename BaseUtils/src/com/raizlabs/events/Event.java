3357c2d3d1589971c2e6c261b0ec9cd177d3ec75
Dylan James <Dylan.James@raizlabs.com>
2013-03-19 15:02:51 -0400

    Thread safety added to Events.

--- BaseUtils/src/com/raizlabs/events/Event.java
+++ BaseUtils/src/com/raizlabs/events/Event.java
@@ -10,12 +10,18 @@ import java.util.HashSet;
  */
 public class Event<T> {
 	private HashSet<EventListener<T>> listeners;
+	private HashSet<EventListener<T>> listenersToAdd;
+	private HashSet<EventListener<T>> listenersToRemove;
+	private boolean raisingEvent;
 	
 	/**
 	 * Creates a new RZEvent
 	 */
 	public Event() {
 		listeners = new HashSet<EventListener<T>>();
+		listenersToAdd = new HashSet<EventListener<T>>();
+		listenersToRemove = new HashSet<EventListener<T>>();
+		raisingEvent = false;
 	}
 	
 	/**
@@ -23,7 +29,17 @@ public class Event<T> {
 	 * @param listener The listener to be notified.
 	 */
 	public void addListener(EventListener<T> listener) {
-		if (listener != null) listeners.add(listener);
+		if (listener != null) {
+			synchronized (this) {
+				// If the event is currently being raised, we can't modify the collection
+				// Add it to the toAdd collection
+				if (raisingEvent) {
+					listenersToAdd.add(listener);
+				} else {
+					listeners.add(listener);
+				}
+			}
+		}
 	}
 	
 	/**
@@ -33,7 +49,16 @@ public class Event<T> {
 	 * @return True if the listener was removed, false if it wasn't found.
 	 */
 	public boolean removeListener(EventListener<T> listener) {
-		return listeners.remove(listener);
+		synchronized (this) {
+			// If the event is currently being raised, we can't modify the collection
+			// Add it to the toRemove collection
+			if (raisingEvent) {
+				listenersToRemove.add(listener);
+				return listeners.contains(listener);
+			} else {
+				return listeners.remove(listener);
+			}
+		}
 	}
 	
 	/**
@@ -42,8 +67,25 @@ public class Event<T> {
 	 * @param args The arguments to the event which will be passed to the listeners.
 	 */
 	public void raiseEvent(Object sender, T args) {
-		for (EventListener<T> listener : listeners) {
-			listener.onEvent(sender, args);
+		synchronized (this) {
+			raisingEvent = true;
+			listenersToAdd.clear();
+			listenersToRemove.clear();
+			for (EventListener<T> listener : listeners) {
+				// Don't raise the event if the listener is flagged to be removed.
+				if (!listenersToRemove.contains(listener)) {
+					listener.onEvent(sender, args);
+				}
+			}
+			
+			for (EventListener<T> listener : listenersToAdd) {
+				listeners.add(listener);
+			}
+			
+			for (EventListener<T> listener : listenersToRemove) {
+				listeners.remove(listener);
+			}
+			raisingEvent = false;
 		}
 	}
 	
@@ -51,6 +93,16 @@ public class Event<T> {
 	 * Clears all listeners from this event.
 	 */
 	public void clear() {
-		listeners.clear();
+		synchronized (this) {
+			// If the event is currently being raised, we can't modify the collection
+			// Add all of the current listeners to the toRemove collection
+			if (raisingEvent) {
+				for (EventListener<T> listener : listeners) {
+					listenersToRemove.add(listener);
+				}
+			} else {
+				listeners.clear();
+			}
+		}
 	}
 }
