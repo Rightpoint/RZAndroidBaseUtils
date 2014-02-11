5425ded5d1ce62e0c4d697394481e61d8962ddf4
Dylan James <Dylan.James@raizlabs.com>
2014-02-11 11:39:57 -0500

    Added String array parsing to JSONHelper

--- BaseUtils/src/com/raizlabs/json/JSONHelper.java
+++ BaseUtils/src/com/raizlabs/json/JSONHelper.java
@@ -7,6 +7,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.text.TextUtils;
 import android.util.Log;
 
 /**
@@ -71,5 +72,52 @@ public class JSONHelper {
 			
 			return items;
 		}
+	}
+	
+	/**
+	 * Parses all strings out of the data at the given key inside the given
+	 * {@link JSONObject} if it exists
+	 * @param json The {@link JSONObject} to look for the key in
+	 * @param key The key to look for the strings under
+	 * @return The list of strings, or null if they couldn't be parsed or the
+	 * key wasn't defined
+	 */
+	public static List<String> parseStrings(JSONObject json, String key) {
+		// Attempt to grab the messages as an array of messages
+		List<String> strings = parseStrings(json.optJSONArray(key));
+		if (strings == null) {
+			// If there is only one string, the field will be a simple string
+			// mapping instead of an array of size 1 :(
+			String string = json.optString(key);
+			if (!TextUtils.isEmpty(string)) {
+				strings = new ArrayList<String>(1);
+				strings.add(string);
+			}
+		}
+
+		return strings;
+	}
+	
+	/**
+	 * Parses all strings out of the given {@link JSONArray} into a list.
+	 * @param json The array to parse.
+	 * @return A list containing all the strings or null if the input array
+	 * was null.
+	 */
+	public static List<String> parseStrings(JSONArray json) {
+		if (json != null) {
+
+			final int numStrings = json.length();
+
+			List<String> strings = new ArrayList<String>(numStrings);
+
+			for (int i = 0; i < numStrings; i++) {
+				String string = json.optString(i);
+				if (!TextUtils.isEmpty(string)) strings.add(string);
+			}
+
+			return strings;
+		}
+		return null;
 	}
 }
